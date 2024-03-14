4fdcbc1ed0bacecae2019e2bf2ff93c4918cb729
moon920830 <lucasdavis0830@gmail.com>
2024-03-14 15:18:31 -0400

    - merge with other codebase

--- Media_Manager/apps/Home/urls.py
+++ Media_Manager/apps/Home/urls.py
@@ -3,10 +3,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index_view, name='index_view'),
+	# path('', views.index_view, name='index_view'),
 	path('login/', views.login_view, name='login_view'),
 	path('forgot/', views.forgot, name='forgot'),
 	path('reset/', views.reset, name='reset'),
 	path('logout/', views.logout_view, name='logout_view'),
 	path('support/', views.support, name='support'),
+
+	path('', views.index, name='index'),
+	path('campaign-dashboard', views.adDashboard, name='adDashboard'),
+	path('ad-rates', views.adRateIndex, name='adRateIndex'),
+	path('new-ad-rate', views.newAdRate, name='newAdRate'),
+	path('completed-rate', views.completedRate, name='completedRate'),
+	path('edit-rate', views.editRate, name='editRate'),
+	path('publication-dashboard', views.pubDashboard, name='pubDashboard'),
+
+	path('create-gl-code', views.newGlCode, name='newGlCode'),
+
+	path('admin/', views.admin, name='admin'),
+	path('admin/general', views.adminGeneral, name='adminGeneral'),
+	path('admin/ads', views.adminAds, name='adminAds'),
+
 ]
--- Media_Manager/apps/Home/views.py
+++ Media_Manager/apps/Home/views.py
@@ -113,3 +113,40 @@ def support(request):
 
 	context = {'message':'', 'groups': ', '.join(views.get_groups(request)), 'menu': views.get_sidebar(request)}
 	return render(request, 'support.html', context)
+
+
+def index(request):
+  return render(request, 'pages/campaigns-home.html')
+
+def adDashboard(request):
+  return render(request, 'pages/campaign-dashboard.html')
+
+def adRateIndex(request):
+  return render(request, 'pages/ad-rates/ad-rates-home.html')
+
+def newAdRate(request):
+  return render(request, 'pages/ad-rates/new-ad-rate.html')
+
+def completedRate(request):
+  return render(request, 'pages/ad-rates/completed-rate.html')
+
+def editRate(request):
+  return render(request, 'pages/ad-rates/edit-rate.html')
+
+def pubDashboard(request):
+  return render(request, 'pages/publications/publication-dashboard.html')
+
+def glDashboard(request):
+  return render(request, 'pages/gl-codes/gl-dashboard.html')
+
+def newGlCode(request):
+  return render(request, 'pages/gl-codes/create-gl-code.html')
+
+def admin(request):
+  return render(request, 'pages/admin/admin.html')
+
+def adminGeneral(request):
+  return render(request, 'pages/admin/admin-general.html')
+
+def adminAds(request):
+  return render(request, 'pages/admin/ads/ads.html')
